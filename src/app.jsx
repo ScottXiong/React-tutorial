164902b0e033c09b815827bf2d6b9feb5ca8b768
scottxiong <github.scott.bear@gmail.com>
2018-02-25 15:33:22 +0800

    little change

--- src/app.jsx
+++ src/app.jsx
@@ -5,10 +5,26 @@ import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import './index.scss';
 
+
+
+let name = "scott";
+let flag = false;
+let names = ['Timmy', 'John', 'Scott', 'Ivy', 'Judy']
+let jsx = <div>Good morning</div>
+
+function Componet() {
+    return <h1>I am {name}</h1>
+}
+
+class Es6Component extends React.Component {
+    constructor(props) {
+      super(props);
+     } 
+    render(){
+        return <h1> Hello, I am {this.props.description}</h1>
+    }
+}
 ReactDOM.render(
-  <div>  
-        <i className="fa fa-apple"></i>
-        <h1>Hello, world!</h1>
-  </div>,
-  document.getElementById('app')
+       <Es6Component description="Scott" />,
+    document.getElementById('app')
 );
